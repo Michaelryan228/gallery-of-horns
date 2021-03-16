ff7f67f934a61b7a7fc2620f9ca2297cc1e3e519
Michael Ryan <michaelryan2895@gmail.com>
2021-03-15 22:06:09 -0400

    header, footer, main, horned-beasts js files

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>&copy; Michael Ryan</h3>
+            </div>
+        );
+    }
+}
+
+export default Footer;
